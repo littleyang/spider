312bbfa3ba43cdd7edc62ca74a82dc9c092bb850
littleyang <littleyangqh@gmail.com>
2016-07-27 12:25:02 +0800

    add spider

--- src/main/java/com/xiaoke/spider/qixinbao/GithubRepoPageProcessor.java
+++ src/main/java/com/xiaoke/spider/qixinbao/GithubRepoPageProcessor.java
@@ -1,16 +1,27 @@
 package com.xiaoke.spider.qixinbao;
 
+import java.text.SimpleDateFormat;
+
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
+/**
+ * Created by fengpj on 2016/7/6.
+ * 企业名录抓取
+ */
 public class GithubRepoPageProcessor implements PageProcessor {
 
-    private Site site = Site.me().setRetryTimes(3).setSleepTime(100);
+    private Site site = Site.me().setTimeOut(10000).setRetryTimes(3).setSleepTime(2000)//.setHttpProxyPool(new HttpProxyGetter().getProxyList(0))
+            .addHeader("User-Agent","Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36");
 
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Override
     public void process(Page page) {
-        page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/\\w+/\\w+)").all());
+    	
+    	page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/\\w+/\\w+)").all());
         page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
         page.putField("name", page.getHtml().xpath("//h1[@class='entry-title public']/strong/a/text()").toString());
         if (page.getResultItems().get("name")==null){
@@ -20,10 +31,11 @@ public class GithubRepoPageProcessor implements PageProcessor {
         page.putField("readme", page.getHtml().xpath("//div[@id='readme']/tidyText()"));
     }
 
+    @Override
     public Site getSite() {
         return site;
     }
-
+    
     public static void main(String[] args) {
         Spider.create(new GithubRepoPageProcessor()).addUrl("https://github.com/code4craft").thread(5).run();
     }
