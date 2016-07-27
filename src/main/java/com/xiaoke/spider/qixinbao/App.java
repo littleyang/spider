9961825cdf3e7ea840c234290278fa50dd0446f2
littleyang <littleyangqh@gmail.com>
2016-07-27 10:27:20 +0800

    init project for spider

---
+++ src/main/java/com/xiaoke/spider/qixinbao/App.java
@@ -0,0 +1,13 @@
+package com.xiaoke.spider.qixinbao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
